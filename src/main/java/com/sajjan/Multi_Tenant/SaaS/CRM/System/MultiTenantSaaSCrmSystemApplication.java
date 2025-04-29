f494550bd0f607f7c0e498f474e58285a8decbab
s1coding <sajjansharmab@gmail.com>
2025-04-29 11:12:22 +0100

    Initial commit

---
+++ src/main/java/com/sajjan/Multi_Tenant/SaaS/CRM/System/MultiTenantSaaSCrmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sajjan.Multi_Tenant.SaaS.CRM.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSaaSCrmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantSaaSCrmSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sajjan/Multi_Tenant/SaaS/CRM/System/MultiTenantSaaSCrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajjan.Multi_Tenant.SaaS.CRM.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantSaaSCrmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
